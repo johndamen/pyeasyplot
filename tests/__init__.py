32f290af3ac94a121c876ab294fc87ad40d16f6c
John Damen <johndamen@Johns-MacBook-Pro.local>
2016-10-06 09:16:58 +0200

    simplified test loading

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,21 +1,37 @@
 import unittest
+import importlib
+import os
+
 
 def run():
+
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    from . import datasets, managers
-    suite.addTest(load_tests(loader, datasets))
-    suite.addTest(load_tests(loader, managers))
+    # load tests from this directory
+    root = os.path.split(__file__)[0]
+    for f in os.listdir(root):
+        # skip private modules
+        if f.startswith('_'):
+            continue
 
+        # get module name from filename
+        if os.path.isdir(os.path.join(root, f)):
+            name = f
+        else:
+            name, ext = os.path.splitext(f)
+            if ext != '.py':
+                continue
+
+        # import module
+        m = importlib.import_module('.'+name, package=__name__)
+
+        # add tests from module
+        module_suite = loader.loadTestsFromModule(m)
+        print('{:20s} {} tests'.format(
+            m.__name__, module_suite.countTestCases()))
+        suite.addTest(module_suite)
+
+    # run tests
     unittest.TextTestRunner().run(suite)
-
-
-def load_tests(loader, m):
-    suite = unittest.TestSuite()
-    for k, v in m.__dict__.items():
-        if isinstance(v, type) and issubclass(v, unittest.TestCase):
-            tests = loader.loadTestsFromTestCase(v)
-            suite.addTests(tests)
-    return suite
 
